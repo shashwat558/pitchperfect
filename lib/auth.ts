aa4196f4acbd2075dd25f84b2c0a39d51c1e106d
shashwat558 <jainshashwat528@gmail.com>
2025-11-08 18:14:23 +0530

    add better-auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,25 @@
+import { PrismaClient } from "@prisma/client";
 import {betterAuth} from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-    
+    database: prismaAdapter(prisma, {
+        provider: "postgresql"
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        google: {
+           clientId: "",
+           clientSecret: ""
+        }
+    },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7
+    }
+
 
 })
