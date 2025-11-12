94a23119fd1ec2bbd41fdd21d7e0347cfedecc14
shashwat558 <jainshashwat528@gmail.com>
2025-11-12 23:37:06 +0530

    added middleware

---
+++ proxy.ts
@@ -0,0 +1,22 @@
+import {NextRequest, NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+
+export async function proxy(request:NextRequest){
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if(!session) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.next();
+
+}
+
+export const config = {
+    runtime: "nodejs",
+    matcher: ["/dashboard"]
+}
