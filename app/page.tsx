c7c493d1dff8e9ac4e4a9be9fda153f783b7ad3c
shashwat558 <jainshashwat528@gmail.com>
2025-11-13 23:16:38 +0530

    fixed an issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>hi</div>
+    <div className="flex min-h-screen items-center justify-center bg-[#0b0b09] font-sans dark:bg-black">
+      
+    </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,44 +49,46 @@ const Navbar = ({className}: {className:string}) => {
                 <h1 className='text-lg font-semibold'>PitchPerfect</h1>
 
             </div>
-            <div className='relative flex justify-center space-x-15 items-center'>
+            <div className='relative flex justify-center space-x-12 items-center'>
                 <h1>Features</h1>
                 <div
-                    className="relative"
+                    className="relative dark:text-[#bcd1af] text-white"
                     
                     onMouseLeave={() => setIsHovered(false)}
                     >
-                    <h1 className='flex items-center gap-1' onMouseEnter={() => setIsHovered(true)}>Pricing <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="icon-sm text-token-text-tertiary"><path d="M12.1338 5.94433C12.3919 5.77382 12.7434 5.80202 12.9707 6.02929C13.1979 6.25656 13.2261 6.60807 13.0556 6.8662L12.9707 6.9707L8.47067 11.4707C8.21097 11.7304 7.78896 11.7304 7.52926 11.4707L3.02926 6.9707L2.9443 6.8662C2.77379 6.60807 2.80199 6.25656 3.02926 6.02929C3.25653 5.80202 3.60804 5.77382 3.86617 5.94433L3.97067 6.02929L7.99996 10.0586L12.0293 6.02929L12.1338 5.94433Z"></path></svg></h1>
-
+                    <h1 className='flex items-center gap-1' onMouseEnter={() => setIsHovered(true)}>Pricing <svg width="10" height="10" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="icon-sm text-token-text-tertiary"><path d="M12.1338 5.94433C12.3919 5.77382 12.7434 5.80202 12.9707 6.02929C13.1979 6.25656 13.2261 6.60807 13.0556 6.8662L12.9707 6.9707L8.47067 11.4707C8.21097 11.7304 7.78896 11.7304 7.52926 11.4707L3.02926 6.9707L2.9443 6.8662C2.77379 6.60807 2.80199 6.25656 3.02926 6.02929C3.25653 5.80202 3.60804 5.77382 3.86617 5.94433L3.97067 6.02929L7.99996 10.0586L12.0293 6.02929L12.1338 5.94433Z"></path></svg></h1>
+                     
                     <motion.div
                         variants={mainContainer}
                         initial="hidden"
                         animate={isHovered ? "show" : "hidden"}
-                        className={`absolute top-10 left-0 rounded-none border border-white/10 bg-black/70 backdrop-blur-md shadow-lg p-2 min-w-[200px]`}
+                        className={`absolute top-5 -left-10 rounded-none  p-2 min-w-[250px]`}
                     >
-                        <motion.ul
+                       <motion.ul
                         variants={container}
                         initial="hidden"
                         animate={isHovered ? "show" : "hidden"}
-                        className="flex flex-col gap-1"
+                        className="grid grid-cols-2 gap-2 mt-4 rounded-2xl border border-white/10 bg-black/60 backdrop-blur-lg shadow-[0_8px_30px_rgba(0,0,0,0.6)] p-3 min-w-[320px]"
                         >
                         {[
-                            { name: "Pro", desc: "For teams scaling fast" },
-                            { name: "Basic", desc: "For individuals" },
-                            { name: "Premium", desc: "Full enterprise suite" },
+                            { name: "Basic", desc: "For individuals starting out" },
+                            { name: "Pro", desc: "For small teams scaling fast" },
+                            { name: "Ultra Pro", desc: "For enterprises & heavy users" },
+                            { name: "Custom", desc: "Tailored solutions on request" },
                         ].map((plan) => (
                             <motion.li
                             key={plan.name}
                             variants={item}
-                            className="group cursor-pointer rounded-lg px-3 py-2 transition-all duration-200 hover:bg-white/10
-                            border-dashed border-gray-300 border-px
-                            "
+                            className={`group  flex flex-col justify-center rounded-xl border border-white/5 bg-white/5 px-4 py-3 hover:border-white/20 hover:bg-white/10 transition-all duration-200 cursor-pointer`}
                             >
-                            <h3 className="text-white text-sm font-medium">{plan.name}</h3>
+                            <h3 className="text-white text-sm font-medium group-hover:text-white">
+                                {plan.name}
+                            </h3>
                             <p className="text-gray-400 text-xs">{plan.desc}</p>
                             </motion.li>
                         ))}
                         </motion.ul>
+
                     </motion.div>
                     </div>
 
