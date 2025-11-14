24ed671c5a5f1b88b0d9a1a3b86bc0cf577df272
shashwat558 <jainshashwat528@gmail.com>
2025-11-14 22:38:52 +0530

    Add ts-node dependency and create seed script for database initialization

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+const {PrismaClient} = require("../lib/generated/prisma")
+
+const pitchTypes = [
+
+]
+
+
+const prisma = new PrismaClient();
+async function main() {
+    const pitches = await prisma.pitchType.createMany({
+        data: pitchTypes
+
+    })
+    console.log(pitches)
+}
+
+main()
+.then(async() => {
+    await prisma.$disconnect()
+})
+.catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
