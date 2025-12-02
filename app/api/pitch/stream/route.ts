9de9f7ac8ad8168d678b74af57109220d016009b
shashwat558 <jainshashwat528@gmail.com>
2025-12-02 22:28:58 +0530

    used ai-sdk

--- app/api/pitch/stream/route.ts
+++ app/api/pitch/stream/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import {createOpenRouter} from "@openrouter/ai-sdk-provider";
-import {streamText} from "ai";
+import {generateObject} from "ai";
 import {z} from "zod";
 import {createClient} from "@deepgram/sdk";
 
@@ -11,6 +11,42 @@ const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 const openRouter = createOpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY
 });
+
+const evaluationFunction = async ({systemPrompt, facials:{eyeContact, smileIntensity}, transcription}: {
+  systemPrompt: string,
+  facials: {
+    eyeContact: number,
+    smileIntensity: number
+  },
+  transcription: string
+}) => {
+
+  const {object} = await generateObject({
+    model: openRouter("x-ai/grok-4.1-fast:free"),
+    schema: z.object({
+      feedback: z.object({
+        score: z.number(),
+        text: z.string().max(60),
+        suggestion: z.string().max(100)
+      })
+    }),
+    schemaDescription: "Feedback for the small part of the pitch",
+    system: systemPrompt,
+    messages: [
+      {
+        role: "user",
+        content: `Transcript: ${transcription},
+                  Eye Contact: ${eyeContact},
+                  smileIntensity: ${smileIntensity},
+                  Give a live feedback 
+        `,
+
+        }
+    ]
+  });
+  console.log(object)
+  return object
+}
 
  
 
@@ -29,13 +65,29 @@ export async function POST(req:NextRequest){
     audioBuffer,
     {
       model: "nova-3",
-      smart_format: true
+      smart_format: true,
+      diarize: true,
+      language: "en-US",
+      punctuate: true,
+      utterances: true,
+      
     }
   );
 
   if(error) throw error;
+ 
 
-  if(!error) console.dir(result, {depth: null})
+  if(!error){
+    const transcript = result.results.channels[0].alternatives[0].transcript;
+    const aiResponse = await evaluationFunction({systemPrompt: systemPrompt, facials: {
+      eyeContact: eyeContactPct,
+      smileIntensity: smileIntensity
+    }, transcription: transcript});
+
+    console.log(aiResponse);
+
+    
+  }
 
 
 
